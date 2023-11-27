29d86bd372fa38e29bff7276c960d7272a16649e
merry <k.maria.m.24.05@gmail.com>
2023-11-27 19:04:21 +0200

    rolling back validation changes

--- src/components/FileUploadForm/validation.js
+++ src/components/FileUploadForm/validation.js
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 
 const VALIDATION_SCHEMA = yup.object().shape({
-    file: yup.string().required('errors.required') });
+    file: yup.string().required('Required') });
 
 export default VALIDATION_SCHEMA;
