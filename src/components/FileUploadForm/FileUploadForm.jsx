42340190907a77bca29e91d0338f92cb8a89dcfb
merry <k.maria.m.24.05@gmail.com>
2023-12-01 10:26:21 +0200

    fix error handling

--- src/components/FileUploadForm/FileUploadForm.jsx
+++ src/components/FileUploadForm/FileUploadForm.jsx
@@ -106,29 +106,12 @@ const convertBase64ToImage = (base64) => {
   });
 };
 
-// const dataURLtoFile = (url, fileNameWithExtension) => {
-//   const arr = url.split(',');
-//   const mime = arr[0].match(/:(.*?);/)[1];
-//   const bstr = atob(arr[1]);
-//   let n = bstr.length;
-//   const u8arr = new Uint8Array(n);
-
-//   while (n--) {
-//     u8arr[n] = bstr.charCodeAt(n);
-//   }
-
-//   return new File([u8arr], fileNameWithExtension, { type: mime });
-// };
 
 const getBlob = async (canvas) => {
   return new Promise((resolve, reject) => {
     try {
       canvas.toBlob((blob) => {
-        if (blob) {
-          resolve(blob);
-        } else {
-          reject(new Error('Failed to get the blob'));
-        }
+        resolve(blob);
       });
     } catch (error) {
       reject(error);
